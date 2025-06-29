66317e7f974745a8e56041d3e176e4cc99c18d2a
Raymond Abiola <raymondintell@gmail.com>
2025-06-29 17:31:53 +0100

    added the mint function and override update in other inherited contracts

--- src/JudgeToken.sol
+++ src/JudgeToken.sol
@@ -6,22 +6,28 @@ import {ERC20Burnable} from "../lib/openzeppelin-contracts/contracts/token/ERC20
 import {ERC20Permit} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {AccessControl} from "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 import {ERC20Capped} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import {Pausable} from "../lib/openzeppelin-contracts/contracts/utils/Pausable.sol";
-import {ReentrancyGuard} from "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuardTransient.sol";
+import {ReentrancyGuard} from "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract JudgeToken is ERC20, ERC20Burnable, ERC20Permit, AccessControl, Pausable, ReentrancyGuard{
+contract JudgeToken is ERC20, ERC20Burnable, ERC20Permit, AccessControl, ERC20Capped, ReentrancyGuard{
 bytes32 constant public MINTER_ROLE = keccak256("MINTER_ROLE");
-bytes32 constant public PAUSER_ROLE =keccak256("PAUSER_ROLE");
+bytes32 constant public PAUSER_ROLE = keccak256("PAUSER_ROLE");
 bytes32 constant public SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
 
-constructor() ERC20 ("JudgeToken", "JUDGE")
+constructor (uint256 initialSupply) 
+ERC20 ("JudgeToken", "JUDGE")
 ERC20Capped(100_000_000*10**decimals())
+ERC20Permit("JudgeToken")
 {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(MINTER_ROLE, msg.sender);
     _grantRole(PAUSER_ROLE, msg.sender);
     _grantRole(SNAPSHOT_ROLE, msg.sender);
+
+    _mint(msg.sender, initialSupply);
 }
 
+function mint(address to, uint amount)external onlyRole(MINTER_ROLE){
+    _mint(to, amount);
+}
 
 }
