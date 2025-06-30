949f63fafc1de92ea3c220b4bf26123557fcdf4b
Raymond Abiola <raymondintell@gmail.com>
2025-06-30 11:24:48 +0100

    overrides for conflicting parent contracts

--- src/JudgeToken.sol
+++ src/JudgeToken.sol
@@ -6,11 +6,11 @@ import {ERC20Burnable} from "../lib/openzeppelin-contracts/contracts/token/ERC20
 import {ERC20Permit} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {AccessControl} from "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 import {ERC20Capped} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import {ReentrancyGuard} from "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import {ERC20Votes} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "../lib/openzeppelin-contracts/contracts/utils/Nonces.sol";
 
-contract JudgeToken is ERC20, ERC20Burnable, ERC20Permit, AccessControl, ERC20Capped, ReentrancyGuard{
+contract JudgeToken is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, AccessControl, ERC20Capped{
 bytes32 constant public MINTER_ROLE = keccak256("MINTER_ROLE");
-bytes32 constant public SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
 
 constructor (uint256 initialSupply) 
 ERC20 ("JudgeToken", "JUDGE")
@@ -19,8 +19,6 @@ ERC20Permit("JudgeToken")
 {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(MINTER_ROLE, msg.sender);
-    _grantRole(SNAPSHOT_ROLE, msg.sender);
-
     _mint(msg.sender, initialSupply);
 }
 
@@ -32,7 +30,11 @@ function setRoleAdmin(bytes32 role, bytes32 adminRole)external onlyRole(DEFAULT_
 _setRoleAdmin(role, adminRole);
 }
 
-function _update(address from, address to, uint value) internal override (ERC20, ERC20Capped){
+function _update(address from, address to, uint value) internal override (ERC20, ERC20Capped, ERC20Votes){
     super._update(from, to, value);
+}
+
+function nonces(address owner) public view override(Nonces) returns(uint256){
+    return super.nonces(owner);
 }
 }
