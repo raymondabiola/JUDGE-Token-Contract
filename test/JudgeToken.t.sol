77583a2cd49146ab2a0a6b966770d8e766897ccf
Raymond Abiola <raymondintell@gmail.com>
2025-07-03 20:24:27 +0100

    added more test functions

--- test/JudgeToken.t.sol
+++ test/JudgeToken.t.sol
@@ -12,8 +12,14 @@ contract JudgeTokenTest is Test{
     address public user2;
     address public user3;
     uint8 decimals = 18;
+    uint256 initialSupply = 1_000_000 * 10 ** uint256(decimals);
 
     error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
+    error ERC20InvalidSender(address sender);
+    error ERC20InvalidReceiver(address receiver);
+    error ERC20InvalidSpender(address spender);
+    error ERC20ExceededCap(uint256 increasedSupply, uint256 cap);
+
 
     function setUp() public{
 owner = address(this);
@@ -21,25 +27,41 @@ zeroAddress = address(0);
 user1 = makeAddr("user1");
 user2 = makeAddr("user2");
 user3 = makeAddr("user3");
-
-uint256 initialSupply = 1_000_000 * 10 ** uint256(decimals);
 judgeToken = new JudgeToken(initialSupply);
     }
 
 function testName()public{
-
+assertEq("JudgeToken", judgeToken.name());
 }
 
 function testSymbol()public{
-
+assertEq("JUDGE", judgeToken.symbol());
 }
 
 function testDecimals()public{
+assertEq(decimals, judgeToken.decimals());
+}
 
+function testCap()public{
+    uint256 cap = 100_000_000 * 10 ** uint256(decimals);
+       uint256 mintAmount = 99_000_001 * 10 ** uint256(decimals);
+       uint256 increasedSupply = mintAmount + initialSupply;
+    assertEq(cap, judgeToken.cap());
+    vm.expectRevert(
+        abi.encodeWithSelector(
+            ERC20ExceededCap.selector,
+            increasedSupply,
+            judgeToken.cap()
+        )
+    );
+    judgeToken.mint(user1, mintAmount);
 }
 
 function testTotalSupply()public{
-
+ uint256 mintAmount = 67_000 * 10 ** uint256(decimals);
+ judgeToken.mint(user1, mintAmount);
+ uint256 expectedTotalSupply = mintAmount + initialSupply;
+assertEq(expectedTotalSupply, judgeToken.totalSupply());
 }
 
     function testDeployerIsOwner()public{
@@ -65,6 +87,14 @@ function testTotalSupply()public{
             )
         );
         judgeToken.mint(user1, mintAmount);
+
+        vm.expectRevert(
+        abi.encodeWithSelector(
+            ERC20InvalidReceiver.selector,
+            address(0)
+        )
+    );
+    judgeToken.mint(zeroAddress, mintAmount);
     }
 
 function testBurn()public{
@@ -74,26 +104,83 @@ function testBurn()public{
         vm.prank(user1);
         judgeToken.burn(burnAmount);
         assertEq(mintAmount - burnAmount, judgeToken.balanceOf(user1));
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ERC20InvalidSender.selector,
+                address(0)
+            )
+        );
+        vm.prank(zeroAddress);
+        judgeToken.burn(burnAmount);
 }
 
 function testBurnFrom()public{
+ uint256 mintAmount = 100_000 * 10 ** uint256(decimals);
+ uint256 allowance = 30_000 * 10 ** uint256(decimals);
+    uint256 burnAmount = 10_000 * 10 ** uint256(decimals);
+    uint256 balanceLeft = mintAmount - burnAmount;
+    judgeToken.mint(user2, mintAmount);
 
+    vm.prank(user2);
+    judgeToken.approve(user1, allowance);
+
+    vm.prank(user1);
+    judgeToken.burnFrom(user2, burnAmount);
+    
+    assertEq(judgeToken.balanceOf(user2), balanceLeft);
 }
 
 function testBalanceOf()public{
-
+uint256 mintAmount = 100_000 * 10 ** uint256(decimals);
+    judgeToken.mint(user2, mintAmount);
+    assertEq(judgeToken.balanceOf(user2), mintAmount);
 }
 
 function testTransfer()public{
+uint256 mintAmount = 100_000 * 10 ** uint256(decimals);
+uint256 amount = 40_000 * 10 ** uint256(decimals);
+    judgeToken.mint(user2, mintAmount);
+
+    vm.prank(user2);
+    judgeToken.transfer(user1, amount);
+    assertEq(judgeToken.balanceOf(user1), amount);
+
+    vm.expectRevert(
+        abi.encodeWithSelector(
+            ERC20InvalidReceiver.selector,
+            address(0)
+        )
+    );
+    vm.prank(user2);
+judgeToken.transfer(zeroAddress, amount);
 
 }
 
 function testAllowance()public{
+ uint256 mintAmount = 100_000 * 10 ** uint256(decimals);
+ uint256 allowance = 30_000 * 10 ** uint256(decimals);
+    judgeToken.mint(user2, mintAmount);
 
+    vm.prank(user2);
+    judgeToken.approve(user1, allowance);
+
+    assertEq(judgeToken.allowance(user2, user1), allowance);
 }
 
 function testApprove()public{
+ uint256 mintAmount = 100_000 * 10 ** uint256(decimals);
+ uint256 allowance = 30_000 * 10 ** uint256(decimals);
+    judgeToken.mint(user2, mintAmount);
 
+    vm.expectRevert(
+        abi.encodeWithSelector(
+            ERC20InvalidSpender.selector,
+            address(0)
+        )
+    );
+vm.prank(user2);
+judgeToken.approve(zeroAddress, allowance);
 }
 
 function testTransferFrom()public{
@@ -121,10 +208,6 @@ function testRenounceRole()public{
 }
 
 function testSupportsInteface()public{
-
-}
-
-function testCap()public{
 
 }
 
