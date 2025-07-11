454dd91243277d31bdcdb168a395e1f221396149
Raymond Abiola <raymondintell@gmail.com>
2025-07-11 20:02:29 +0100

    added more functions

--- src/JudgeStaking.sol
+++ src/JudgeStaking.sol
@@ -14,22 +14,35 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
     uint256 public lastRewardBlock;
     uint256 public totalStaked;
     uint256 private constant SCALE = 1e18;
+    address[] internal users;
+    bool public emergencyFuncCalled;
+
     struct userStake {
         uint256 amountStaked;
         uint256 rewardDebt;
     }
-    mapping(address => userStake) usersInfo;
+
+    mapping(address => userStake) internal usersInfo;
+    mapping(address => bool) internal isUser;
     bytes32 DEFAULT_ADMIN_ROLE = keccak256("DEFAULT_ADMIN_ROLE");
 
     event RewardsFunded(uint256 amount);
     event Deposited(address indexed addr, uint256 amount);
     event Withdrawn(address indexed addr, uint256 amount);
+    event EmergencyWithdrawal(
+        address indexed admin,
+        address indexed user,
+        uint256 stakeWithdrawn,
+        uint256 rewardPaid
+    );
 
     error InvalidAmount();
     error InsufficientBal();
+    error AlreadyTriggered();
 
-    constructor(address _judgeTokenAddress) {
+    constructor(address _judgeTokenAddress, uint256 _rewardPerBlock) {
         judgeToken = JudgeToken(_judgeTokenAddress);
+        rewardPerBlock = _rewardPerBlock;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
@@ -37,6 +50,12 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
         require(amount > 0, InvalidAmount());
         judgeToken.safeTransferFrom(msg.sender, address(this), amount);
         emit RewardsFunded(amount);
+    }
+
+    function setNewRewardPerBlock(
+        uint256 newRewardPerBlock
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        rewardPerBlock = newRewardPerBlock;
     }
 
     function updatePool() internal {
@@ -55,6 +74,10 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
     function deposit(uint256 _amount) external {
         require(_amount > 0, InvalidAmount());
         userStake storage user = usersInfo[msg.sender];
+        if (!isUser[msg.sender]) {
+            users.push(msg.sender);
+            isUser[msg.sender] = true;
+        }
         updatePool();
 
         if (user.amountStaked > 0) {
@@ -99,13 +122,61 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
             user.rewardDebt;
         if (pending > 0) {
             judgeToken.safeTransfer(msg.sender, pending);
-
-            uint256 amountWithdrawn = user.amountStaked;
-            user.amountStaked = 0;
-            totalStaked -= amountWithdrawn;
-            user.rewardDebt = 0;
-            judgeToken.safeTransfer(msg.sender, amountWithdrawn);
-            emit Withdrawn(msg.sender, amountWithdrawn);
         }
+
+        uint256 amountWithdrawn = user.amountStaked;
+        user.amountStaked = 0;
+        totalStaked -= amountWithdrawn;
+        user.rewardDebt = 0;
+        judgeToken.safeTransfer(msg.sender, amountWithdrawn);
+        emit Withdrawn(msg.sender, amountWithdrawn);
+    }
+
+    function emergencyWithdraw()
+        external
+        nonReentrant
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(!emergencyFuncCalled, AlreadyTriggered());
+        emergencyFuncCalled = true;
+        for (uint256 i; i < users.length; i++) {
+            address userAddr = users[i];
+            uint256 amount = usersInfo[users[i]].amountStaked;
+
+            updatePool();
+            if (usersInfo[userAddr].amountStaked > 0) {
+                uint256 pending = (usersInfo[userAddr].amountStaked *
+                    accJudgePerShare) /
+                    SCALE -
+                    usersInfo[userAddr].rewardDebt;
+
+                judgeToken.safeTransfer(userAddr, pending);
+
+                usersInfo[userAddr].amountStaked = 0;
+                totalStaked -= amount;
+                usersInfo[userAddr].rewardDebt = 0;
+                judgeToken.safeTransfer(userAddr, amount);
+                emit EmergencyWithdrawal(msg.sender, userAddr, amount, pending);
+            }
+        }
+    }
+
+    function myStakeDetails() external view returns (userStake memory) {
+        return usersInfo[msg.sender];
+    }
+
+    function getUserList()
+        external
+        view
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        returns (address[] memory)
+    {
+        return users;
+    }
+
+    function getUserStakeDetails(
+        address addr
+    ) external view onlyRole(DEFAULT_ADMIN_ROLE) returns (userStake memory) {
+        return usersInfo[addr];
     }
 }
