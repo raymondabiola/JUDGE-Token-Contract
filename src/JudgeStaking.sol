0de96b806f6aadc7efc24faec1ed1e28d32e35ce
Raymond Abiola <raymondintell@gmail.com>
2025-07-12 14:09:58 +0100

    refactored code to include lockup periods and multiple stakes per user

--- src/JudgeStaking.sol
+++ src/JudgeStaking.sol
@@ -9,20 +9,29 @@ import {JudgeToken} from "./JudgeToken.sol";
 contract JudgeStaking is AccessControl, ReentrancyGuard {
     using SafeERC20 for JudgeToken;
     JudgeToken public judgeToken;
+    uint256 private newStakeId;
     uint256 public accJudgePerShare;
     uint256 public rewardPerBlock;
     uint256 public lastRewardBlock;
-    uint256 public totalStaked;
+    uint256 public totalCalculatedStakeForReward;
+    uint public totalStaked;
     uint256 private constant SCALE = 1e18;
     address[] internal users;
+    uint256 private constant maxLockUpPeriod = 360;
     bool public emergencyFuncCalled;
 
     struct userStake {
+        uint256 id;
         uint256 amountStaked;
+        uint256 lockUpPeriod;
+        uint256 lockUpRatio;
+        uint256 calculatedStakeForReward;
+        uint256 depositTimestamp;
         uint256 rewardDebt;
+        uint256 maturityTimestamp;
     }
 
-    mapping(address => userStake) internal usersInfo;
+    mapping(address => userStake[]) internal userStakes;
     mapping(address => bool) internal isUser;
     bytes32 DEFAULT_ADMIN_ROLE = keccak256("DEFAULT_ADMIN_ROLE");
 
@@ -40,9 +49,12 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
     error InvalidAmount();
     error InsufficientBal();
     error AlreadyTriggered();
+    error NotYetMatured();
+    error InvalidLockUpPeriod();
 
     constructor(address _judgeTokenAddress, uint256 _rewardPerBlock) {
         judgeToken = JudgeToken(_judgeTokenAddress);
+        newStakeId = 1;
         rewardPerBlock = _rewardPerBlock;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
@@ -64,72 +76,99 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
         if (block.number <= lastRewardBlock) {
             return;
         }
-        if (totalStaked == 0) {
+        if (totalCalculatedStakeForReward == 0) {
             lastRewardBlock = block.number;
         }
         uint256 blocksPassed = block.number - lastRewardBlock;
         uint256 totalReward = blocksPassed * rewardPerBlock;
-        accJudgePerShare += (totalReward * SCALE) / totalStaked;
+        accJudgePerShare +=
+            (totalReward * SCALE) /
+            totalCalculatedStakeForReward;
         lastRewardBlock = block.number;
     }
 
-    function deposit(uint256 _amount) external {
+    function deposit(uint256 _amount, uint256 _lockUpPeriodInDays) external {
         require(_amount > 0, InvalidAmount());
-        userStake storage user = usersInfo[msg.sender];
+        require(_lockUpPeriodInDays <= maxLockUpPeriod, InvalidLockUpPeriod());
         if (!isUser[msg.sender]) {
             users.push(msg.sender);
             isUser[msg.sender] = true;
         }
-        updatePool();
 
-        if (user.amountStaked > 0) {
-            uint256 pending = (user.amountStaked * accJudgePerShare) /
-                SCALE -
-                user.rewardDebt;
-            if (pending > 0) {
-                judgeToken.safeTransfer(msg.sender, pending);
-            }
-        }
+        updatePool();
+        uint256 amountStaked = _amount;
+        uint256 lockUpPeriod = _lockUpPeriodInDays;
+        uint256 lockUpRatio = (lockUpPeriod * SCALE) / maxLockUpPeriod;
+        uint256 calculatedStakeForReward = (amountStaked * lockUpRatio) / SCALE;
+        uint256 depositTimestamp = block.timestamp;
+        uint256 rewardDebt = (calculatedStakeForReward * accJudgePerShare) /
+            SCALE;
+        uint256 maturityTimestamp = depositTimestamp + lockUpPeriod;
+
+        userStake memory newStake = userStake({
+            id: newStakeId,
+            amountStaked: amountStaked,
+            lockUpPeriod: lockUpPeriod,
+            lockUpRatio: lockUpRatio,
+            calculatedStakeForReward: calculatedStakeForReward,
+            depositTimestamp: depositTimestamp,
+            rewardDebt: rewardDebt,
+            maturityTimestamp: maturityTimestamp
+        });
+
         judgeToken.safeTransferFrom(msg.sender, address(this), _amount);
+        userStakes[msg.sender].push(newStake);
+
+        totalCalculatedStakeForReward += calculatedStakeForReward;
         totalStaked += _amount;
-        user.amountStaked += _amount;
-        user.rewardDebt = (user.amountStaked * accJudgePerShare) / SCALE;
+
+        newStakeId++;
         emit Deposited(msg.sender, _amount);
     }
 
-    function withdraw(uint256 _amount) external nonReentrant {
+    function withdraw(uint256 _amount, uint256 _index) external nonReentrant {
         require(_amount > 0, InvalidAmount());
-        userStake storage user = usersInfo[msg.sender];
-        require(_amount <= user.amountStaked, InsufficientBal());
+        userStake storage stake = userStakes[msg.sender][_index];
+        require(stake.maturityTimestamp <= block.timestamp, NotYetMatured());
+        require(_amount <= stake.amountStaked, InsufficientBal());
 
         updatePool();
-        uint256 pending = (user.amountStaked * accJudgePerShare) /
+        uint256 pending = (stake.calculatedStakeForReward * accJudgePerShare) /
             SCALE -
-            user.rewardDebt;
+            stake.rewardDebt;
         if (pending > 0) {
             judgeToken.safeTransfer(msg.sender, pending);
         }
-        user.amountStaked -= _amount;
+        totalCalculatedStakeForReward -= stake.calculatedStakeForReward;
+        stake.amountStaked -= _amount;
+        stake.calculatedStakeForReward =
+            (stake.amountStaked * stake.lockUpRatio) /
+            SCALE;
+        totalCalculatedStakeForReward += stake.calculatedStakeForReward;
+        stake.rewardDebt =
+            (stake.calculatedStakeForReward * accJudgePerShare) /
+            SCALE;
         totalStaked -= _amount;
-        user.rewardDebt = (user.amountStaked * accJudgePerShare) / SCALE;
         judgeToken.safeTransfer(msg.sender, _amount);
         emit Withdrawn(msg.sender, _amount);
     }
 
-    function withdrawAll() external nonReentrant {
-        userStake storage user = usersInfo[msg.sender];
+    function withdrawAll(uint _index) external nonReentrant {
+        userStake storage stake = userStakes[msg.sender][_index];
         updatePool();
-        uint256 pending = (user.amountStaked * accJudgePerShare) /
+        uint256 pending = (stake.calculatedStakeForReward * accJudgePerShare) /
             SCALE -
-            user.rewardDebt;
+            stake.rewardDebt;
         if (pending > 0) {
             judgeToken.safeTransfer(msg.sender, pending);
         }
 
-        uint256 amountWithdrawn = user.amountStaked;
-        user.amountStaked = 0;
+        uint256 amountWithdrawn = stake.amountStaked;
+        totalCalculatedStakeForReward -= stake.calculatedStakeForReward;
+        stake.amountStaked = 0;
+        stake.calculatedStakeForReward = 0;
         totalStaked -= amountWithdrawn;
-        user.rewardDebt = 0;
+        stake.rewardDebt = 0;
         judgeToken.safeTransfer(msg.sender, amountWithdrawn);
         emit Withdrawn(msg.sender, amountWithdrawn);
     }
@@ -143,20 +182,20 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
         emergencyFuncCalled = true;
         for (uint256 i; i < users.length; i++) {
             address userAddr = users[i];
-            uint256 amount = usersInfo[users[i]].amountStaked;
+            uint256 amount = userStakes[users[i]].amountStaked;
 
             updatePool();
-            if (usersInfo[userAddr].amountStaked > 0) {
-                uint256 pending = (usersInfo[userAddr].amountStaked *
+            if (userStakes[userAddr].amountStaked > 0) {
+                uint256 pending = (userStakes[userAddr].amountStaked *
                     accJudgePerShare) /
                     SCALE -
-                    usersInfo[userAddr].rewardDebt;
+                    userStakes[userAddr].rewardDebt;
 
                 judgeToken.safeTransfer(userAddr, pending);
 
-                usersInfo[userAddr].amountStaked = 0;
+                userStakes[userAddr].amountStaked = 0;
                 totalStaked -= amount;
-                usersInfo[userAddr].rewardDebt = 0;
+                userStakes[userAddr].rewardDebt = 0;
                 judgeToken.safeTransfer(userAddr, amount);
                 emit EmergencyWithdrawal(msg.sender, userAddr, amount, pending);
             }
@@ -164,7 +203,7 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
     }
 
     function myStakeDetails() external view returns (userStake memory) {
-        return usersInfo[msg.sender];
+        return userStakes[msg.sender];
     }
 
     function getUserList()
@@ -179,6 +218,6 @@ contract JudgeStaking is AccessControl, ReentrancyGuard {
     function getUserStakeDetails(
         address addr
     ) external view onlyRole(DEFAULT_ADMIN_ROLE) returns (userStake memory) {
-        return usersInfo[addr];
+        return userStakes[addr];
     }
 }
